b7940f82ddcdcb966e5a0b4c527a2b27da89959e
Gridin_Andrei <andreigridin918@gmail.com>
2023-07-01 21:27:49 +0500

    Chenged methods name on CreatedArray

--- Array_transform/Program.cs
+++ Array_transform/Program.cs
@@ -3,18 +3,18 @@ using static System.Console;
 
 Clear();
 
-string[] WriteAndRead()
+string[] CreatedArray()
 {
     Write("Введите строку из четырёх или более слов: ");
     string words = ReadLine()!;
     string[] array = words.Split(' ');
-    while(array.Length < 3)
+    while (array.Length < 4)
     {
-        Write("Вы ввели менее четырёх слов.");
-        WriteAndRead();
+        Write("Вы ввели менее четырёх слов.\nПопробуйте ещё ввести строку из четырёх или более слов: ");
+        words = ReadLine()!;
+        array = words.Split(' ');
     }
     return array;
 }
-
 
 
