34d791e58980036b226b4b4d709352c4440394c0
Gridin_Andrei <andreigridin918@gmail.com>
2023-07-01 20:10:58 +0500

    Created method WriteAndRead()

---
+++ Array_transform/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] WriteAndRead()
+{
+    Write("Введите строку из четырёх или более слов: ");
+    string words = ReadLine()!;
+    string[] array = words.Split(' ');
+    while(array.Length < 3)
+    {
+        Write("Вы ввели менее четырёх слов.");
+        WriteAndRead();
+    }
+    return array;
+}
+
+
+
